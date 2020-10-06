2bfeb9f72991caebcd2e9107edc7328e8044632e
Amad Ali <amad.ali@sandc.com>
2020-10-05 21:48:50 -0500

    Explained MIME-TYPES in the comments

--- src/MiniWebServer.java
+++ src/MiniWebServer.java
@@ -2,6 +2,30 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/*
+
+    As per the requirements of the checklist, here are the answers to the requested questions!
+
+    Q1) Explained how MIME-types are used to tell the browser what data is coming.
+
+    A1) The following answer was reached from reading Stack Overflow/Wikipedia/FireFox Documentation.
+
+        A MIME type is a label used to identify a type of data. If the browser see's an ID like "application/mp3",
+        it knows to the data incoming is an MP3 and it needs to launch some application to handle it, ust like it would
+        a pdf or some other specialized file format. I thought a clever explanation I saw was like referring to MIME-type
+        as like a file extention for the internet browser. It see's an ID and then knows what it needs to do to handle
+        that properly.
+
+    Q2) Explained how you would return the contents of requested files (web pages) of type HTML (text/html)
+
+    A2)
+
+    Q3) Explained how you would return the contents of requested files (web pages) of type TEXT (text/plain)
+
+    A3)
+ */
+
+
 //From the sample provided code, ListenWorker is Worker
 class Worker extends Thread {
 
